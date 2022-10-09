4f829a36cdce0f80e272f273717414c8e793e67c
maks141 <books141@mail.ru>
2022-10-09 17:42:20 +0500

    Интерфейс

--- Фт-210007 Спиридонов.py
+++ Фт-210007 Спиридонов.py
@@ -1,20 +1,40 @@
 from Lab_2 import lab_2
 
-employees = int(input('Введите количество сотрудников: '))
+# Входные данные
+while True:
+    employees = int(input('Введите количество сотрудников: '))
+    if employees > 0:
+        break
+    else:
+        print('Ввод должен содержать числа, большие чем 0')
 
+# Входные данные
 kilometers = []
 for i in range(1, employees + 1):
-    kilometer = int(input('Введите количество километров для {} сотрудника: '.format(i)))
+    while True:
+        kilometer = int(input('Введите количество километров для {} сотрудника: '.format(i)))
+        if kilometer > 0:
+            break
+        else:
+            print('Ввод должен содержать числа, большие чем 0')
     kilometers.append(kilometer)
 
-
+# Входные данные
 taxi = []
 for i in range(1, employees + 1):
-    price = int(input('Введите цену за километр у {} такси: '.format(i)))
+    while True:
+        price = int(input('Введите цену за километр у {} такси: '.format(i)))
+        if price > 0:
+            break
+        else:
+            print('Ввод должен содержать числа, большие чем 0')
     taxi.append(price)
 
+# Cортировка списков
 max_kilometers = sorted(kilometers, reverse=True)
 min_taxi = sorted(taxi)
+
+# Выбор такси для сотрудника
 counter = 0
 for i in kilometers:
     index_km = max_kilometers.index(i)
@@ -22,6 +42,7 @@ for i in kilometers:
     counter += 1
     print('Сотрудник {} - такси №{}'.format(counter, num_taxi + 1))
 
+# Конечная сумма
 data_sum = []
 for i in range(len(max_kilometers)):
     summ = max_kilometers[i] * min_taxi[i]
